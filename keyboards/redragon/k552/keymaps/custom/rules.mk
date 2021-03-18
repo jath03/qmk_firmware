7197c3a24fe93fa3576d5c43042ef48b76aca506
jath03 <jackthughes@yahoo.com>
2021-03-18 14:39:20 -0700

    moving custom stuff to a keymap

---
+++ keyboards/redragon/k552/keymaps/custom/rules.mk
@@ -0,0 +1,21 @@
+BOOTMAGIC_ENABLE = no # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no    # Mouse keys
+EXTRAKEY_ENABLE = no    # Audio control and System control
+CONSOLE_ENABLE = no     # Console for debug
+COMMAND_ENABLE = no     # Commands for debug and configuration
+SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes        # USB Nkey Rollover
+AUDIO_ENABLE = no
+RGBLIGHT_ENABLE = no
+SERIAL_LINK_ENABLE = no
+WAIT_FOR_USB = no
+CUSTOM_MATRIX = yes
+# DEBOUNCING_TYPE = sym_eager_pk
+
+RAW_ENABLE = yes
+OPENRGB_ENABLE = yes
+OPENRGB_USE_CUSTOM_MATRIX_MAP = yes
+
+# Custom RGB matrix handling
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = custom
