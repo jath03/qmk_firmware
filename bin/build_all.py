f09dae24e0682c03655ef81ebfce7a58dfa5e2f9
jath03 <jackthughes@yahoo.com>
2021-03-23 13:06:02 -0700

    [Redragon k552] added @jack-bkr's keymap

--- bin/build_all.py
+++ bin/build_all.py
@@ -5,3 +5,5 @@ BOARDS = ['redragon/k552', 'redragon/k556', 'keychron/k4', 'keychron/k6']
 
 for kb in BOARDS:
     subprocess.run(f"bin/qmk compile -kb {kb} -km default -j{os.cpu_count()}", shell=True)
+    subprocess.run(f"bin/qmk compile -kb {kb} -km iso -j{os.cpu_count()}", shell=True)
+
