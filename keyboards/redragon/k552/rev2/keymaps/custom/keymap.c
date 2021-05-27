6525139ab104be247096fb8449af5a50b1b9b2e6
jath03 <jackthughes@yahoo.com>
2021-05-26 21:27:34 -0700

    Adding a numpad

--- keyboards/redragon/k552/rev2/keymaps/custom/keymap.c
+++ keyboards/redragon/k552/rev2/keymaps/custom/keymap.c
@@ -28,7 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // entirely and just use numbers.
 enum layer_names {
     _BASE,
-    _CONTROLS
+    _CONTROLS,
+    _NUMPAD
 };
 
 enum my_keycodes {
@@ -40,12 +41,12 @@ enum my_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*  Row:        0                       1           2           3       4           5        6       7           8           9           10          11          12          13          14          15          16          17          18          19          20     */
     [_BASE] = LAYOUT_ansi(
-                KC_ESC,                             KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_PSCR,    KC_SLCK,    RGB_TOG,
-                KC_GRV,                 KC_1,       KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    KC_INS,     KC_HOME,    KC_PGUP,
-                KC_TAB,                 KC_Q,       KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_DEL,     KC_END,     KC_PGDN,
+                KC_ESC,                             KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_PSCR,   TO(_NUMPAD), RGB_TOG,
+                KC_GRV,                 KC_1,       KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    KC_INS,    KC_HOME,     KC_PGUP,
+                KC_TAB,                 KC_Q,       KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_DEL,    KC_END,      KC_PGDN,
                 LT(_CONTROLS, KC_CAPS), KC_A,       KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,                KC_ENT,
-                KC_LSPO,                            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,                KC_RSPC,                KC_UP,
-                KC_LCTL,                KC_LGUI,    KC_LALT,                            KC_SPC,                                      KC_RALT,    KC_RGUI,    KC_APP,     KC_RCTL,    KC_LEFT,    KC_DOWN,    KC_RIGHT
+                KC_LSPO,                            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,                KC_RSPC,               KC_UP,
+                KC_LCTL,                KC_LGUI,    KC_LALT,                            KC_SPC,                                      KC_RALT,    KC_RGUI,    KC_APP,     KC_RCTL,    KC_LEFT,   KC_DOWN,     KC_RIGHT
             ),
     [_CONTROLS] = LAYOUT_ansi(
                 _______,                   _______,       _______,   _______,       _______,    _______,   _______,       _______,       _______,       _______,     _______,    _______,     _______,     RGB_RMOD,    RGB_MOD,    _______,
@@ -54,7 +55,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 _______,    _______,       _______,       _______,   _______,       _______,    _______,   _______,       _______,       _______,       _______,     _______,                 LGO_MOD,
                 _______,                   _______,       _______,   _______,       _______,    _______,   _______,       _______,       _______,       _______,     _______,                 _______,                  KC_VOLU,
                 _______,    _______,       _______,                                             KC_MPLY,                                                _______,     _______,    _______,     WLK_AWY,     KC_MPRV,     KC_VOLD,    KC_MNXT
-                )
+            ),
+    [_NUMPAD] = LAYOUT_ansi(
+            _______,                   _______,       _______,   _______,       _______,    _______,   _______,       _______,       _______,       _______,     _______,    _______,     TG(_NUMPAD), KC_P7,       KC_P8,      KC_P9,
+            _______,    _______,       _______,       _______,   _______,       _______,    _______,   _______,       _______,       _______,       _______,     _______,    _______,     KC_BSPC,     KC_P4,       KC_P5,      KC_P6,
+            _______,    _______,       _______,       _______,   _______,       _______,    _______,   _______,       _______,       _______,       _______,     _______,    _______,     KC_NO,       KC_P1,       KC_P2,      KC_P3,
+            _______,    _______,       _______,       _______,   _______,       _______,    _______,   _______,       _______,       _______,       _______,     _______,                 KC_NO,
+            _______,                   _______,       _______,   _______,       _______,    _______,   _______,       _______,       _______,       _______,     _______,                 KC_P0,                    KC_UP,
+            _______,    _______,       _______,                                              KC_SPC,                                                _______,     _______,    _______,     KC_NO,       KC_LEFT,     KC_DOWN,    KC_RIGHT
+    )
 };
 
 
