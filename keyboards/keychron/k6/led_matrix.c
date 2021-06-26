d5d49883f0b74a11dcf1df5a7f535005aed2df64
jath03 <jackthughes@yahoo.com>
2021-06-26 16:26:36 -0700

    build this!

--- keyboards/keychron/k6/led_matrix.c
+++ keyboards/keychron/k6/led_matrix.c
@@ -92,7 +92,7 @@ void led_set(uint8_t usb_led) {
     }
 }
 */
-
+// Why won't the auto-build run?
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if (res && led_state.caps_lock) {
